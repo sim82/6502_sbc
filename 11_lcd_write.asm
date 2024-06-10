b0f33fa5277ce0b54b4a6d2c769327339c037e3f
Simon A. Berger <simberger@gmail.com>
2024-06-10 19:39:17 +0200

    WIP: reorder io space

--- 11_lcd_write.asm
+++ 11_lcd_write.asm
@@ -6,17 +6,24 @@
 	.BYTE $55
 	
 .CODE
+	lda #$01
+	sta $0001
 reset:
 	lda #$f
-	sta $e001
+	sta $e011
 	lda #$1
-	sta $e001
+	sta $e011
 	ldx #$0
 start:
 	lda message, X
 	beq reset
-	sta $e002
+	sta $e012
 	inx
+	lda $0001
+	rol
+	adc #$0
+	sta $0001
+	sta $e000
 	jmp start
 .RODATA
 message:
