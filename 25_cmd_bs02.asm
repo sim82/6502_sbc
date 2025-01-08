18b5b104b669f7dc53d30c1e01de42d5778274da
Simon A. Berger <simberger@gmail.com>
2025-01-08 18:28:42 +0100

    WIP: switch to 16bit

--- 25_cmd_bs02.asm
+++ 25_cmd_bs02.asm
@@ -6,9 +6,15 @@ IP = $90 ; meh, bootloaded clobbers ZP + $80... TODO: move it down to $00 to pre
 JMP_ADDR = IP + 2
 MEM_OPERAND = JMP_ADDR + 2
 TMP_A = MEM_OPERAND + 1
+OP1L = TMP_A + 1
+OP1H = OP1L + 1
+OP2L = OP1H + 1
+OP2H = OP2L + 1
 
 .BSS
 variables:
+	.RES $100
+variables_high:
 	.RES $100
 
 code:
@@ -58,18 +64,45 @@ opcode_table:
 	.WORD op_break			; $00
 	.WORD op_store_immediate	; $02
 	.WORD op_add_immediate		; $04
-	.WORD $0000			; $06
+	.WORD op_store_immediate16	; $06
 	.WORD op_print			; $08
 	.WORD $0000			; $0a
 	.WORD $0000			; $0c
 	.WORD $0000			; $0e
-	.WORD op_bne			; $10
-	.WORD op_beq			; $12
-	.WORD op_bmi			; $14
-	.WORD op_bpl			; $16
+	.WORD op_bne16			; $10
+	.WORD 0000			; $12
+	.WORD op_blt16			; $14
+	.WORD 0000			; $16
 
 op_break:
 	rts
+
+op_bne16:
+	jsr branch_setup2
+	lda OP1H
+	cmp OP2H
+	bne take_branch
+	lda OP1L
+	cmp OP2L
+	bne take_branch
+	jmp normal_return
+
+op_blt16:
+	jsr branch_setup2
+	lda OP1H
+	cmp OP2H
+	bcc take_branch
+	bne normal_return
+	lda OP1L
+	cmp OP2L
+	bcc take_branch
+	jmp normal_return
+
+
+take_branch:
+	lda code, y
+	sta IP
+	jmp mod_ip_return
 
 op_store_immediate:
 	iny
@@ -77,8 +110,22 @@ op_store_immediate:
 	iny
 	lda code, y
 	sta variables, x
+	lda #$00
+	sta variables_high, x
 	jmp normal_return
 
+op_store_immediate16:
+	iny
+	ldx code, y
+	iny
+	lda code, y
+	sta variables, x
+	iny
+	lda code, y
+	sta variables_high, x
+	jmp normal_return
+
+	
 op_add_immediate:
 	iny
 	ldx code, y
@@ -87,52 +134,39 @@ op_add_immediate:
 	clc
 	adc variables, x
 	sta variables, x
+	lda variables_high, x
+	adc #$00
+	sta variables_high, x
 	jmp normal_return
 
-branch_setup:
+branch_setup2:
 	iny
 	ldx code, y
 	lda variables, x
-	sta TMP_A ; first operand goes into A for comparison. save on stack
+	sta OP1L
+	lda variables_high, x
+	sta OP1H
 
 	iny
 	ldx code, y
 	lda variables, x
-	; sta MEM_OPERAND ; second operand goes into MEM for comparison.
-	sta MEM_OPERAND
+	sta OP2L
+	lda variables_high, x
+	sta OP2H
 	iny
-	lda TMP_A
-	cmp MEM_OPERAND
 	rts
+	
 
-op_bne:
-	jsr branch_setup
-	bne op_branch_do_jmp
-	jmp normal_return
-op_beq:
-	jsr branch_setup
-	beq op_branch_do_jmp
-	jmp normal_return
-op_bmi:
-	jsr branch_setup
-	bmi op_branch_do_jmp
-	jmp normal_return
-op_bpl:
-	jsr branch_setup
-	bpl op_branch_do_jmp
-	jmp normal_return
-
-op_branch_do_jmp:
-	lda code, y
-	sta IP
-	jmp mod_ip_return
 
 
 op_print:
 	iny
 	ldx code, y
 	lda variables, x
-	ldx #$00
+	sta TMP_A
+	lda variables_high, x
+	tax
+	lda TMP_A
 	jsr os_print_dec
 	jsr os_putnl
 	iny
