876e4a8e1189ab4fdd6fc55ee5813c51dff7ce76
Simon A. Berger <simberger@gmail.com>
2025-04-19 20:26:19 +0200

    scale factor can just be calculated as float and rounded in the end...

--- mimonify/src/bin/ramp.rs
+++ mimonify/src/bin/ramp.rs
@@ -17,28 +17,48 @@ fn main() {
             let p = SAMPLE_RATE / freq;
             const OVERRIDE: bool = !true;
 
-            let m = if OVERRIDE || p >= 128.0 {
-                let pi = p as u32;
-                slope(0..=pi, 0..=256, 0u32)
-            } else if p >= 64.0 {
-                let pi = (p * 2.0) as u32;
-                slope(0..=pi, 0..=256, 1u32)
-            } else if p >= 32.0 {
-                let pi = (p * 4.0) as u32;
-                slope(0..=pi, 0..=256, 2u32)
-            } else if p >= 16.0 {
-                let pi = (p * 8.0) as u32;
-                slope(0..=pi, 0..=256, 3u32)
-            } else {
-                let pi = (p * 16.0) as u32;
-                slope(0..=pi, 0..=256, 4u32)
+            let (scale, shift) = match p {
+                ..16.0 => (16.0, 4),
+                ..32.0 => (8.0, 3),
+                ..64.0 => (4.0, 2),
+                ..128.0 => (2.0, 1),
+                _ => (1.0, 0),
             };
-            println!("{}, {}: {}", freq, p, m);
-            m
+            // let m = if OVERRIDE || p >= 128.0 {
+            //     let pi = p as u32;
+            //     slope(0..=pi, 0..=256, 0u32)
+            // } else if p >= 64.0 {
+            //     let pi = (p * 2.0) as u32;
+            //     slope(0..=pi, 0..=256, 1u32)
+            // } else if p >= 32.0 {
+            //     let pi = (p * 4.0) as u32;
+            //     slope(0..=pi, 0..=256, 2u32)
+            // } else if p >= 16.0 {
+            //     let pi = (p * 8.0) as u32;
+            //     slope(0..=pi, 0..=256, 3u32)
+            // } else {
+            //     let pi = (p * 16.0) as u32;
+            //     slope(0..=pi, 0..=256, 4u32)
+            // };
+
+            let x1 = (p * scale) as u32;
+            let m = slope(0..=x1, 0..=256, shift);
+            let mf = 1.0 / p;
+            println!(
+                "{}, {}, {}, {}: {}, {}",
+                freq,
+                p,
+                scale,
+                shift,
+                m,
+                mf * 256.0 * 256.0
+            );
+            // m
+            (mf * 256.0 * 256.0) as u32
         })
         .collect::<Vec<_>>();
     // let mut m = slope(0..=90, 0..=256); //((y1 - y0) << 8) / (x1 - x0);
-    let m = params[8];
+    let m = params[0];
 
     println!("m: {}", m);
     let mut y = 0;
