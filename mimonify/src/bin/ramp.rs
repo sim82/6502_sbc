9d8cdda5fbe56d9237d0da15587de7577a00efdb
Simon A. Berger <simberger@gmail.com>
2025-04-19 20:40:08 +0200

    cleanup

--- mimonify/src/bin/ramp.rs
+++ mimonify/src/bin/ramp.rs
@@ -15,44 +15,9 @@ fn main() {
         .iter()
         .map(|freq| {
             let p = SAMPLE_RATE / freq;
-            const OVERRIDE: bool = !true;
 
-            let (scale, shift) = match p {
-                ..16.0 => (16.0, 4),
-                ..32.0 => (8.0, 3),
-                ..64.0 => (4.0, 2),
-                ..128.0 => (2.0, 1),
-                _ => (1.0, 0),
-            };
-            // let m = if OVERRIDE || p >= 128.0 {
-            //     let pi = p as u32;
-            //     slope(0..=pi, 0..=256, 0u32)
-            // } else if p >= 64.0 {
-            //     let pi = (p * 2.0) as u32;
-            //     slope(0..=pi, 0..=256, 1u32)
-            // } else if p >= 32.0 {
-            //     let pi = (p * 4.0) as u32;
-            //     slope(0..=pi, 0..=256, 2u32)
-            // } else if p >= 16.0 {
-            //     let pi = (p * 8.0) as u32;
-            //     slope(0..=pi, 0..=256, 3u32)
-            // } else {
-            //     let pi = (p * 16.0) as u32;
-            //     slope(0..=pi, 0..=256, 4u32)
-            // };
-
-            let x1 = (p * scale) as u32;
-            let m = slope(0..=x1, 0..=256, shift);
             let mf = 1.0 / p;
-            println!(
-                "{}, {}, {}, {}: {}, {}",
-                freq,
-                p,
-                scale,
-                shift,
-                m,
-                mf * 256.0 * 256.0
-            );
+            println!("{}, {}: {}", freq, p, mf * 256.0 * 256.0);
             // m
             mf
         })
